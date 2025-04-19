bf98ae62526e313501d8ebff4be51d9d187747c2
ronaldsitio <ronaldsitio@gmail.com>
2025-04-20 00:23:25 +0700

    Add screens/FinanceScreen.js 1745083405243

---
+++ screens/FinanceScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function FinanceScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Finance Screen (to be implemented)</Text>
+    </View>
+  );
+}
