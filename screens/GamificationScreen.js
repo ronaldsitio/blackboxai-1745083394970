07e9eb6624b1ade041dd9bb5eb8fe537019f48c4
ronaldsitio <ronaldsitio@gmail.com>
2025-04-20 00:23:24 +0700

    Add screens/GamificationScreen.js 1745083404012

---
+++ screens/GamificationScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function GamificationScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Gamification Screen (to be implemented)</Text>
+    </View>
+  );
+}
